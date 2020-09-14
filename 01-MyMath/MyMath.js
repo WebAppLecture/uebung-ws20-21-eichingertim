8ddfb5fbd97f6b059f8f61463900b69e8f7dbe51
Tim Eichinger <privat.timeichinger@gmail.com>
2020-09-14 12:03:19 +0200

    Solved 01-MyMath

--- 01-MyMath/MyMath.js
+++ 01-MyMath/MyMath.js
@@ -15,30 +15,49 @@ export class MyMath {
         // 'this' referenziert den Kontext in dem die aktuelle Funktion aufgerufen wird. 
         // Hier referenziert es die Instanz der Klasse MyMath die wir gerade erstellen.
         // mit 'value * 1' erzwingen wir, dass value als number gelesen wird.
-        this.value = value * 1; 
+
+        this.value = (value == null) ? this.value = 0 : this.value = value * 1;
     }
 
     add(value) {
-
+        if (value != null) this.value += value;
     }
 
     subtract(value) {
-
+        if (value != null) this.value -= value;
     }
 
     multiply(value) {
-
+        if (value != null) this.value *= value;
     }
 
     divide(value) {
-
+        if (value != null) this.value = (value !== 0) ? this.value / value : this.value;
+            
     }
 
     pow(value) {
-
+        if (value != null && value >= 0) {
+            if (value === 0) this.value = 1;
+            else if (value === 1) this.value = this.value;
+            else {
+                let begin = this.value;
+                for (let i = 1; i < value; i++) this.value *= begin;
+            }
+            
+        }
     }
 
     faculty() {
-
+        if (this.value % 1 === 0) {
+            if (this.value === 0 || this.value === 1) this.value = 1; 
+            else {
+                var num = this.value;
+                while (num > 1) { 
+                    num--;
+                    this.value *= num; 
+                }
+            }
+        }
     }
 }
